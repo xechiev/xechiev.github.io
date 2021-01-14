cb92597e1fb484d1aa11768ea625483b0b2a5cfb
Mingiian Khechiev <58348380+xechiev@users.noreply.github.com>
2021-01-14 19:27:46 +0300

    Update script.js

--- Task_1.5/script.js
+++ Task_1.5/script.js
@@ -1,18 +1,19 @@
+'use strict';
 const slider = document.querySelector('.swiper-container');
 let mySwiper;
 
 const btn = document.querySelector('.brand-button');
-let result = document.querySelectorAll('.hidden1120');
-let res768 = document.querySelectorAll('.hidden768');
-let checkBox = document.querySelector('.expand');
+// let result = document.querySelectorAll('.hidden1120');
+// let res768 = document.querySelectorAll('.hidden768');
+// let checkBox = document.querySelector('.expand');
 
 
 function mobileSlider() {
     if(window.innerWidth < 768 && slider.dataset.mobile === 'false') {
         mySwiper = new Swiper(slider, {
-            slidesPerView: 1,
-            width: 256,
+            slidesPerView: 'auto',
             centeredSlides: true,
+            width: 240,
             pagination: {
                 el: '.swiper-pagination',
                 clickable: true,
@@ -39,31 +40,31 @@ window.addEventListener('resize', function() {
     mobileSlider();
 });
 
-btn.addEventListener("click", function() {
-    if(window.innerWidth >= 1120) {
-         for(let i=0; i <result.length; i++) {
-            if(result[i].classList.contains('hidden')) {
-                btn.textContent = " Скрыть";
-             } else {
-                btn.textContent = "Показать все";
-             }
-          result[i].classList.toggle('hidden');
-        }
-    }
-});
-btn.addEventListener("click", function() {
-    if(window.innerWidth >= 768 && window.innerWidth <= 1119) {
-         for(let i=0; i < res768.length; i++) {
-            if(res768[i].classList.contains('hidden')) {
-                btn.textContent = " Скрыть";
-             } else {
-                btn.textContent = "Показать все";
-             }
-             res768[i].classList.toggle('hidden');
-        }
-    }
-});
-let toogleCheckbox = function() {
-    checkBox.checked = !checkBox.checked;
-};
-btn.addEventListener('click', toogleCheckbox);
+// btn.addEventListener("click", function() {
+//     if(window.innerWidth >= 1120) {
+//          for(let i=0; i <result.length; i++) {
+//             if(result[i].classList.contains('hidden')) {
+//                 btn.textContent = " Скрыть";
+//              } else {
+//                 btn.textContent = "Показать все";
+//              }
+//           result[i].classList.toggle('hidden');
+//         }
+//     }
+// });
+// btn.addEventListener("click", function() {
+//     if(window.innerWidth >= 768 && window.innerWidth <= 1119) {
+//          for(let i=0; i < res768.length; i++) {
+//             if(res768[i].classList.contains('hidden')) {
+//                 btn.textContent = " Скрыть";
+//              } else {
+//                 btn.textContent = "Показать все";
+//              }
+//              res768[i].classList.toggle('hidden');
+//         }
+//     }
+// });
+// let toogleCheckbox = function() {
+//     checkBox.checked = !checkBox.checked;
+// };
+// btn.addEventListener('click', toogleCheckbox);
