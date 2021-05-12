89e1d242bc713fdfaddeb3c379edcf4ff28540b9
Mingiian Khechiev <58348380+xechiev@users.noreply.github.com>
2021-05-12 12:32:06 +0300

    Update layout.js

--- 4.2.5/module/layout.js
+++ 4.2.5/module/layout.js
@@ -10,7 +10,6 @@ export class LAYOUT {
 
     this.input = this.createElement('input', 'input-text');
     this.input.placeholder = 'Искать здесь';
-    this.input.id = 'input-text';
 
     this.box_main.append(this.input)
     this.box_main.append(this.box_1)
@@ -21,24 +20,21 @@ export class LAYOUT {
 
   createElement(elementTag, elementClass) {
     const element = document.createElement(elementTag);
-    if(elementClass) {
-      element.classList.add(elementClass)
-    }
+    if(elementClass) element.classList.add(elementClass)
     return element;
   }
 
   createAccounts(accountData) {
     const accountElement = this.createElement('li', 'account'); 
     accountElement.addEventListener('click', () => this.showAccountsOther(accountData));
-   // accountElement.addEventListener('click', () => this.removeButton());
-    accountElement.innerHTML = `<span class='acc-name'>${accountData.name}</span>`                         
-   
+ 
+    accountElement.innerHTML = `<span class='acc-name'>${accountData.name}</span>`;
+
     this.box_1.append(accountElement);
-    
   }
 
-  showAccountsOther(accountData, isUpdate = false) {
-    if(!isUpdate) {
+  showAccountsOther(accountData, condition = false) {
+    if(!condition) {
       this.input.value = '';
       this.box_1.innerHTML = '';
     }
@@ -47,30 +43,36 @@ export class LAYOUT {
     crossImg.src = './img/cross.svg'; 
 
     const accountsItems = this.createElement('div', 'account_box2'); 
-    const  accountsItemsWrapper = this.createElement('div', 'accountsItemsWrapper');
+    const accountsItemsWrapper = this.createElement('div', 'accountsItemsWrapper');
     
     accountsItems.innerHTML = `<div class='show_box2'>Name: ${accountData.name}</div> 
                                <div class='show_box2'>Owner: ${accountData.owner.login}</div> 
                                <div class='show_box2'>Stars: ${accountData.stargazers_count}</div>`;
 
-
-    
-    
     accountsItemsWrapper.append(accountsItems); 
     accountsItemsWrapper.append(crossImg);
-    this.box_2.append(accountsItemsWrapper);    
 
-    this.removeButton() 
-    
+    this.box_2.prepend(accountsItemsWrapper);   
+
+    this.removeChildNodes();
+
+    this.removeButton();
   }
 
   removeButton() {
     this.box_2.addEventListener('click', (event) => {
       if (event.target.className != 'remove_button') return;
-      let del = event.target.closest('.accountsItemsWrapper');
-      del.remove()
+      let deleteCross = event.target.closest('.accountsItemsWrapper');
+      deleteCross.remove();
     })
 
+  }
+
+  removeChildNodes() {
+    let childElements = this.box_2.children.length;
+    if(childElements > 3) { 
+      this.box_2.removeChild(this.box_2.lastChild)
+    }
   }
 
 }
